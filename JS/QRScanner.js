56fb57550a8466d10d2602185011823da024f296
EdsonA22 <zS20017852@estudiantes.uv.mx>
2026-05-10 02:13:02 -0600

    initial commit

---
+++ JS/QRScanner.js
@@ -0,0 +1,31 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: { width: 250, height: 250 } },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer back camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+html5QrCode.stop().then((ignore) => {
+    // QR Code scanning is stopped.
+}).catch((err) => {
+    // Stop failed, handle it.
+});
---
+++ README.md.txt
@@ -0,0 +1,1 @@
+Sistema para la academia de baloncesto Halcones UV
